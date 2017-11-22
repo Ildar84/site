721d55dd2be8b3d7f109357ac02d109748e9e27f
ildar84 <shakirov.i@gmail.com>
2017-11-22 12:57:11 +0300

    favicon add

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var errorHandler = require('errorhandler');
 const browserSync = require('browser-sync');
 var credentials = require('./credentials.js');
 var session = require('express-session');
-
+const favicon = require('express-favicon');
 var mongoose = require('mongoose');
 
 var emailjs = require('emailjs-com');
@@ -19,7 +19,7 @@ emailjs.init("user_XkcTd5cwRkg153yV9e18g");
 var Printer = require('./model/products.js');
 var Bottle = require('./model/bottles.js');
 var Cart = require('./model/cart.js');
-
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.set('port', config.get("port"));
 
 app.use(express.static('public'));
